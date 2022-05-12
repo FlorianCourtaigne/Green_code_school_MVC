91d7c86b5a6ce5e4d9217558661395bb66dd91be
Florian COURTAIGNE <f.courtaigne@gmail.com>
2022-05-12 23:00:01 +0200

    recycle map working

--- src/Controller/RecycleSpotsController.php
+++ src/Controller/RecycleSpotsController.php
@@ -4,22 +4,21 @@ namespace App\Controller;
 
 use Symfony\Component\HttpClient\HttpClient;
 
-// $arrondissements = [75001, 75002, 75003, 75004, 75005, 75006, 75007,
-// 75008, 75009, 75010, 75011, 75012, 75013, 75014, 75015, 75016, 75017, 75018, 75019, 75020];
-
-
 class RecycleSpotsController extends AbstractController
 {
-    public function get()
+    public function getRecycle()
     {
-        $arrondissements = ['75012'];
+        $arrondissement = 75020;
+        if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['submit'])) {
+            $arrondissement = $_GET["district"];
+        }
         $client = HttpClient::create();
-        $urlApi = "https://opendata.paris.fr/api/records/1.0/search/?dataset=dechets-menagers-points-dapport-volontaire-stations-trilib&q=&rows=50&facet=date_de_mise_en_service&facet=code_postal&facet=etat&refine.code_postal=75020"; // phpcs:ignore
+        $urlApi = "https://opendata.paris.fr/api/records/1.0/search/?dataset=dechets-menagers-points-dapport-volontaire-stations-trilib&q=&rows=50&facet=date_de_mise_en_service&facet=code_postal&facet=etat&refine.code_postal=" .$arrondissement; // phpcs:ignore
         $response = $client->request('GET', $urlApi);
         $responseAsAnArray = $response->toArray();
         // var_dump($responseAsAnArray['records'][0]);die;
         return $this->twig->render('RecycleSpots/index.html.twig', [
-            "spots" => $responseAsAnArray, "arrondissements" => $arrondissements
+            "spots" => $responseAsAnArray, "arrondissement" => $arrondissement
         ]);
     }
 }
