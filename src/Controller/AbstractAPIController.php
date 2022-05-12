b5f27cccd702e039e47daa9a0024de549f9118db
Florian COURTAIGNE <f.courtaigne@gmail.com>
2022-05-12 14:40:57 +0200

    first commit

---
+++ src/Controller/AbstractAPIController.php
@@ -0,0 +1,15 @@
+<?php
+
+// Tac's job
+namespace Recycle\Controller;
+
+abstract class AbstractAPIController
+{
+    public function __construct()
+    {
+        header('Content-Type: application/json');
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: POST,GET,PUT,DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    }
+}
